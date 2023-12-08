edb5ee3cdefda4a9281fd2b2ae2e78cfbadb16d1
sinanazem <sinanaazem@gmail.com>
2023-12-08 11:53:14 +0000

    update

--- config/blog/urls.py
+++ config/blog/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, detail
+from .views import home, detail, about, contact
 
 app_name = "blog"
 urlpatterns = [
     path('', home, name="blog"),
     path('<int:post_id>/', detail),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
 ]
--- config/blog/views.py
+++ config/blog/views.py
@@ -8,6 +8,17 @@ def home(requests):
     context = {"articles": articles}
     return render(requests, "blog/index.html", context=context)
 
+def about(requests):
+    articles = Article.objects.all()
+    context = {"articles": articles}
+    return render(requests, "blog/about.html", context=context)
+
+
+def contact(requests):
+    articles = Article.objects.all()
+    context = {"articles": articles}
+    return render(requests, "blog/contact.html", context=context)
+
 def detail(requests, post_id):
     article = Article.objects.get(pk=post_id)
     context = {"article": article}
